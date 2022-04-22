99b0c373586de36539d36949b2c7cdb17b29ab7b
Tamerlan <tamerlan-aliyev-2021@mail.ru>
2022-04-22 12:24:37 +0400

    start1

--- Valyuta/index.js
+++ Valyuta/index.js
@@ -1,7 +1,31 @@
-fetch('https://api.exchangerate.host/latest?base=RUB&symbols=USD ')
-.then((res)=>{
-    return res.json()
+// fetch('https://api.exchangerate.host/latest?base=RUB&symbols=USD ')
+// .then((res)=>{
+//     return res.json()
+// })
+// .then((data)=>{
+//     console.log(data)
+// })
+// btnLeft=document.querySelectorAll('.value-buttons-left button')
+
+// btnLeft.forEach((item,index)=>{
+//         item.addEventListener('click',(e)=>{
+//         e.target.style.background='#833AE0'
+//         e.target.style.color='white'
+//         console.log(e.target)
+//         console.log(index)
+//     })
+// })
+btnLeft=document.querySelectorAll('.value-buttons-left')
+btnLeft[0].firstElementChild.addEventListener('click',()=>{
+    btnLeft[0].firstElementChild.style.background="#833AE0"
+    btnLeft[0].firstElementChild.style.color="white"
+    btnLeft[0].lastElementChild.style.background="#E5E5E5"
+    btnLeft[0].lastElementChild.style.color="black"
 })
-.then((data)=>{
-    console.log(data)
+btnLeft[0].lastElementChild.addEventListener('click',()=>{
+    btnLeft[0].lastElementChild.style.background="#833AE0"
+    btnLeft[0].lastElementChild.style.color="white"
+    btnLeft[0].firstElementChild.style.background="#E5E5E5"
+    btnLeft[0].firstElementChild.style.color="black"
 })
+
