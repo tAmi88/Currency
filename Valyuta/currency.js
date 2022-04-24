8f8c0577c6bd27ff22dbb7de2504f6650630cbd1
Tamerlan <tamerlan-aliyev-2021@mail.ru>
2022-04-24 23:46:59 +0400

    start5

--- Valyuta/currency.js
+++ Valyuta/currency.js
@@ -1,15 +1,16 @@
 let base = "RUB";
 let symbols = "USD";
 let btnLeft = document.querySelectorAll(".value-buttons-left button");
+let btnLeftText = document.querySelectorAll(".value-buttons-left .actived");
+let btnRightText = document.querySelectorAll(".value-buttons-right .actived");
 let btnRight = document.querySelectorAll(".value-buttons-right button");
 let inpLeft = document.querySelector(".inp-left");
 let inpRight = document.querySelector(".inp-right");
-let pLeft=document.querySelector(".p-left")
-let pRight=document.querySelector(".p-right")
+let pLeft = document.querySelector(".p-left");
+let pRight = document.querySelector(".p-right");
 function Style() {
   btnLeft.forEach((item) => {
     item.addEventListener("click", () => {
-      base = item.innerHTML;
       btnLeft.forEach((item) => {
         item.classList.remove("actived");
       });
@@ -20,7 +21,6 @@ function Style() {
   });
   btnRight.forEach((item) => {
     item.addEventListener("click", () => {
-      symbols = item.innerHTML;
       btnRight.forEach((item) => {
         item.classList.remove("actived");
       });
@@ -31,76 +31,102 @@ function Style() {
   });
 }
 Style();
-document.addEventListener("keyup", (e) => {
-  if (e.target == inpRight) {
-    btnLeft.forEach((item) => {
-      item.addEventListener("click", () => {
-        base = item.innerHTML;
-        FechLeft(base, symbols);
-      });
+inpLeft.addEventListener("input", () => {
+  FechRight(base, symbols);
+  btnRight.forEach((item) => {
+    item.addEventListener("click", () => {
+      symbols = item.innerHTML;
+      FechRight(base, symbols);
     });
-  } 
-  else {
-    btnRight.forEach((item) => {
-      item.addEventListener("click", () => {
-        symbols = item.innerHTML;
-        FechRight(base, symbols);
-      });
-    });
-  }
-  Style();
+  });
 });
-
+inpRight.addEventListener("input", () => {
+  FechLeft(base, symbols);
+  btnLeft.forEach((item) => {
+    item.addEventListener("click", () => {
+      base = item.innerHTML;
+      FechLeft(base, symbols);
+    });
+  });
+});
+// btnLeft.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     base = item.innerHTML;
+//     FechRight(base, symbols);
+//   });
+// });
 function FechLeft(baseFunc, symbolsFunc) {
   if (baseFunc != symbolsFunc) {
-    FechLeftText(baseFunc,symbolsFunc)
+    FechLeftText(baseFunc, symbolsFunc);
   }
 }
 function FechRight(baseFunc, symbolsFunc) {
   if (baseFunc != symbolsFunc) {
-    FechRightText(baseFunc, symbolsFunc)
+    FechRightText(baseFunc, symbolsFunc);
   }
 }
 
-function FechLeftText(baseFunc,symbolsFunc){
+function FechLeftText(baseFunc, symbolsFunc) {
   fetch(
     `https://api.exchangerate.host/latest?base=${symbolsFunc}&symbols=${baseFunc}`
   )
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    inpLeft.value = inpRight.value * data.rates[`${baseFunc}`];
-    pRight.innerHTML=`1${data.base}=${data.rates[`${baseFunc}`]}${baseFunc}`
-    fetch(
-      `https://api.exchangerate.host/latest?base=${baseFunc}&symbols=${symbolsFunc}`
-    )
     .then((res) => {
       return res.json();
     })
-    .then((data)=>{
-      pLeft.innerHTML=`1${data.base}=${data.rates[`${symbolsFunc}`]}${symbolsFunc}`
-    })
-  });
+    .then((data) => {
+      inpLeft.value = inpRight.value * data.rates[`${baseFunc}`];
+      pRight.innerHTML = `1${data.base}=${data.rates[`${baseFunc}`]}${baseFunc}`;
+      fetch(
+        `https://api.exchangerate.host/latest?base=${baseFunc}&symbols=${symbolsFunc}`
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          pLeft.innerHTML = `1${data.base}=${data.rates[`${symbolsFunc}`]}${symbolsFunc}`;
+        });
+    });
 }
-function FechRightText(baseFunc, symbolsFunc){
+function FechRightText(baseFunc, symbolsFunc) {
   fetch(
     `https://api.exchangerate.host/latest?base=${baseFunc}&symbols=${symbolsFunc}`
   )
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    inpRight.value = inpLeft.value * data.rates[`${symbolsFunc}`];
-    pLeft.innerHTML=`1${data.base}=${data.rates[`${symbolsFunc}`]}${symbolsFunc}`
-    fetch(
-      `https://api.exchangerate.host/latest?base=${symbolsFunc}&symbols=${baseFunc}`
-    )
     .then((res) => {
       return res.json();
     })
-    .then((data)=>{
-      pRight.innerHTML=`1${data.base}=${data.rates[`${baseFunc}`]}${baseFunc}`
-    })
-  });
+    .then((data) => {
+      inpRight.value = inpLeft.value * data.rates[`${symbolsFunc}`];
+      console.log(data);
+      pLeft.innerHTML = `1${data.base}=${data.rates[`${symbolsFunc}`]}${symbolsFunc}`;
+      fetch(
+        `https://api.exchangerate.host/latest?base=${symbolsFunc}&symbols=${baseFunc}`
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          pRight.innerHTML = `1${data.base}=${data.rates[`${baseFunc}`]}${baseFunc}`;
+        });
+    });
 }
+
+// document.addEventListener("keyup", (e) => {
+//   if (e.target == inpRight) {
+//     FechLeft(base, symbols);
+//     btnLeft.forEach((item) => {
+//       item.addEventListener("click", () => {
+//         base = item.innerHTML;
+//         FechLeft(base, symbols);
+//       });
+//     });
+//   } else {
+//     FechRight(base, symbols);
+//     btnRight.forEach((item) => {
+//       item.addEventListener("click", () => {
+//         symbols = item.innerHTML;
+//         FechRight(base, symbols);
+//       });
+//     });
+//   }
+//   Style();
+// });
